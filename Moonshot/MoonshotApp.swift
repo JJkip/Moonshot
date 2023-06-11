d1e12cbb907d1f6fda8ab5e5cc71e9ce362157ea
jjkip <jjlangatjose@gmail.com>
2023-06-11 23:29:49 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Joseph Langat on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
