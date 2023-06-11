bf2b0a3a2d1775f5c6032a72bb7730d24ce03149
jjkip <jjlangatjose@gmail.com>
2023-06-12 00:34:41 +0300

    Using lazyHstack to load views on demnd horizontally

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -22,14 +22,14 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 10) {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
                 ForEach(0..<100){
                     CustomText("Item \($0)")
                         .font(.title)
                 }
             }
-            .frame(maxWidth: .infinity)
+//            .frame(maxWidth: .infinity)
         }
 //        GeometryReader { geo in
 //            Image("Example")
