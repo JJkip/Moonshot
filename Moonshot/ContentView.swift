28e2c2188d421ca0fac4af9c98b983439d8adf92
jjkip <jjlangatjose@gmail.com>
2023-06-15 17:20:04 +0300

    Testing Automatic Grammar Agreement

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,11 +10,17 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
+    @State private var count = 1
     var body: some View {
         VStack {
-            Text("\(astronauts.count)")
-                .padding()
+//            Text("\(astronauts.count)")
+//                .padding()
+            Button("Add") { count += 1 }
+            .padding(30)
+            Button("Subtract"){ count -= 1 }
+            Text("^[\(count) Cow](inflect: true)")
+                .padding(30)
+            
         }
         .padding()
     }
