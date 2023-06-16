24aaa931b25911898c2a8333e94329e0a12bb86e
jjkip <jjlangatjose@gmail.com>
2023-06-16 19:33:10 +0300

    loading images and their related titles and dates into a grid view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,21 +10,46 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    @State private var count = 1
+//    @State private var count = 1
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     var body: some View {
-        VStack {
-//            Text("\(astronauts.count)")
-//                .padding()
-            Button("Add") { count += 1 }
-            .padding(30)
-            Button("Subtract"){ count -= 1 }
-            Text("^[\(count) Cow](inflect: true)")
-                .padding(30)
+            NavigationView {
+                ScrollView {
+                    LazyVGrid(columns: columns){
+                        ForEach(missions){ mission in
+                            NavigationLink {
+                                Text("Detail View")
+                            } label: {
+                                VStack {
+                                    Image(mission.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                    VStack {
+                                        Text(mission.displayName)
+                                            .font(.caption)
+                                        Text(mission.launchDate ?? "N/A")
+                                            .font(.caption)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                }
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("Moonshot")
+            }
+//            Button("Add") { count += 1 }
+//            .padding(30)
+//            Button("Subtract"){ count -= 1 }
+//            Text("^[\(count) Cow](inflect: true)")
+//                .padding(30)
             
         }
-        .padding()
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
