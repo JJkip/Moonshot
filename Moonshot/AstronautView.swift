0302b6827d50dbfb625f2f11d2a9bb0c4a163cdf
jjkip <jjlangatjose@gmail.com>
2023-06-18 02:16:25 +0300

    aaded AstronautView

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,34 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Joseph Langat on 18/06/2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrog"]!)
+    }
+}
